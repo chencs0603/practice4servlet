62767c0cc438efecb9f49d222393c202d7759f2b
chencs <chencs0603@gmail.com>
2018-02-02 10:09:38 +0800

    hello程序中添加ServletConfig用法

---
+++ helloworld/src/main/java/personal/chencs/practice/servlet/ConfigServlet.java
@@ -0,0 +1,33 @@
+package personal.chencs.practice.servlet;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.OutputStream;
+
+/**
+ * @author: chencs
+ * @date: 2018/2/2
+ * @description:
+ */
+public class ConfigServlet extends HttpServlet {
+
+//    private ServletConfig config;
+//
+//    @Override
+//    public void init(ServletConfig config) throws ServletException {
+//        this.config = config;
+//    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String data = getInitParameter("data");
+
+        OutputStream output = resp.getOutputStream();
+        output.write(data.getBytes());
+    }
+
+}
