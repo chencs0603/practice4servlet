dccf9e5bf933695bf15cbac8183dcf09007cec28
chencs <chencs0603@gmail.com>
2018-03-08 10:23:59 +0800

    重写URL：当浏览器禁用cookie时，通过URL带上jessionid来实现session

---
+++ session/src/main/java/personal/chencs/practice/session/WelcomeServlet.java
@@ -0,0 +1,33 @@
+package personal.chencs.practice.session;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * @author: chencs
+ * @date: 2018/3/8
+ * @description:
+ */
+@WebServlet("/welcome")
+public class WelcomeServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        PrintWriter out = resp.getWriter();
+
+        req.getSession();
+
+        // 重写URL，当浏览器禁用cookie时，可以通过URL带上jsessionid来实现session
+        String cartURL = resp.encodeURL("/cart");
+        String buyURL = resp.encodeURL("/buy");
+
+        out.print("<a href='" + cartURL + "'>加入购物车</a>");
+        out.print("<a href='" + buyURL + "'>结算</a>");
+    }
+
+}
